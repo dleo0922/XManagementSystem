5cb29ff05013ff72fb05fa0965d2f5be34becfce
82103 <82103@NAM>
2020-03-18 19:40:52 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("*** Steam Game Management Menu ***");
+		System.out.println("1. Add game");
+		System.out.println("2. Delet game");
+		System.out.println("3. Edit game");
+		System.out.println("4. View game");
+		System.out.println("5. Exit game");
+		System.out.println("Select one number between 1-5:");
+	}
+
+}
