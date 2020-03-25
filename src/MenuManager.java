6b274954b23ca9b7150315eb2ad9d803e8320962
82103 <82103@NAM>
2020-03-25 21:02:54 +0900

    word revise

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -17,18 +17,18 @@ public class MenuManager { //menu manager 클래스를 만든다.
 			System.out.println("Select one number between 1 - 6:"); //번호 5개 중 하나를 고르게 한다.
 			num = input.nextInt();
 			if (num == 1) {
-				addStudent();
+				addGame();
 			}
 			
 			else if (num == 2) {
-				deletStudent();
+				deletGame();
 			}
 			
 			else if (num == 3) {
-				editStudent();
+				editGame();
 			}
 			else if (num == 4) {
-				viewStudent();
+				viewGame();
 			}
 			else {
 				continue;
@@ -36,12 +36,12 @@ public class MenuManager { //menu manager 클래스를 만든다.
 		}
 	}
 	
-	public static void addStudent() {
+	public static void addGame() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Student ID:");
+		System.out.print("Game ID:");
 		int studentId = input.nextInt();
 		
-		System.out.print("Student name:");
+		System.out.print("Game name:");
 		String studentName = input.next();
 		
 		System.out.print("Email address:");
@@ -54,24 +54,24 @@ public class MenuManager { //menu manager 클래스를 만든다.
 
 	}
 	
-	public static void deletStudent() {
+	public static void deletGame() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Student ID:");
-		int studentId = input.nextInt();
+		System.out.print("Game ID:");
+		int gameId = input.nextInt();
 
 	}
 
-	public static void editStudent() {
+	public static void editGame() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Student ID:");
-		int studentId = input.nextInt();
+		System.out.print("Game ID:");
+		int gameId = input.nextInt();
 
 	}
 	
-	public static void viewStudent() {
+	public static void viewGame() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Student ID:");
-		int studentId = input.nextInt();
+		System.out.print("Game ID:");
+		int gameId = input.nextInt();
 
 	}
 
